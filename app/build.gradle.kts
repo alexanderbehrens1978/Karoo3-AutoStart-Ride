e72abb9a23bbdd490c9c9346bd3ef3c46c9b2af3
axell <alex@ubuntu.ubuntu-domain>
2025-09-07 14:23:27 +0200

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
   namespace = "de.alex.autostartride"
-  compileSdk = 34
+  compileSdk = 35
 
   defaultConfig {
     applicationId = "de.alex.autostartride"
     minSdk = 26
-    targetSdk = 34
+    targetSdk = 35
     versionCode = 1
     versionName = "1.0.0"
   }
